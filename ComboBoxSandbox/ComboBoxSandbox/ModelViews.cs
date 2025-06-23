47dae8942a780bdcb7fea84b1b8f61828415fd92
vpenades <5433822+vpenades@users.noreply.github.com>
2025-06-24 00:47:09 +0200

    first commit

---
+++ ComboBoxSandbox/ComboBoxSandbox/ModelViews.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComboBoxSandbox
+{
+    partial class FormMVVM : ObservableObject
+    {
+        [ObservableProperty]
+        string currentSelection;
+    }
+
+    struct ItemMVVM
+    {
+        public ItemMVVM(string internalValue, string displayValue)
+        {
+            ActualValueToBeSet = internalValue;
+            LocalizedDisplayText = displayValue;
+        }
+
+
+        public string ActualValueToBeSet { get; set; } = "2";
+        public string LocalizedDisplayText { get; set; }
+    }
+}
