859eb8010e837ec3d273305547bbf9a282060151
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2025-10-08 18:11:36 +0200

    nuget++ testing signing

--- HelloWorldCross/HelloWorldCross.Desktop/PublishAll.sh
+++ HelloWorldCross/HelloWorldCross.Desktop/PublishAll.sh
@@ -3,9 +3,9 @@ dotnet tool restore
 dotnet publish /property:PublishProfile="DesktopProfile"
 
 dotnet publish /property:PublishProfile="MacOs64Profile"
-dotnet MacOsAppBundler -p bin/publish/MacOs/x64 -n HelloWorld --id com.rehametrics.helloworld
+dotnet MacOsAppBundler -p bin/publish/MacOs/x64 -n HelloWorld --id com.rehametrics.helloworld --certificate-name "Mac Developer: Vicente Penades (3CT5433VZF)"
 
 dotnet publish /property:PublishProfile="MacOsArmProfile"
-dotnet MacOsAppBundler -p bin/publish/MacOs/arm -n HelloWorld --id com.rehametrics.helloworld
+dotnet MacOsAppBundler -p bin/publish/MacOs/arm -n HelloWorld --id com.rehametrics.helloworld --certificate-name "Mac Developer: Vicente Penades (3CT5433VZF)"
 
 read -p "pause"
