0f44428d27aa63c73fe51ad9f641128649abc2f1
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2025-12-15 17:16:16 +0100

    Wip with shared assets

--- HelloWorldCross/HelloWorldCross/ViewModels/MainViewModel.cs
+++ HelloWorldCross/HelloWorldCross/ViewModels/MainViewModel.cs
@@ -2,8 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using Microsoft.Maui.Storage;
 
 namespace HelloWorldCross.ViewModels;
 
@@ -27,6 +35,9 @@ public partial class MainViewModel : ViewModelBase
         list.Add(new StringPair("Temp", System.IO.Path.GetTempPath()));
         list.Add(new StringPair("App BaseDirectory", AppContext.BaseDirectory ?? "?"));
         list.Add(new StringPair("App ProcessPath", Environment.ProcessPath ?? "?"));
+
+        list.Add(new StringPair("MauiEssentials Cache", FileSystemEx.Current.CacheDirectory));
+        list.Add(new StringPair("MauiEssentials AppData", FileSystemEx.Current.AppDataDirectory));
 
         return list.OrderBy(item => item.Value);
     }
@@ -70,7 +81,76 @@ public partial class MainViewModel : ViewModelBase
     }
 
 
+    [RelayCommand]
+    public async Task LoadResourcesAsync()
+    {
+        using var s = await OpenAppFileAsync("asset0.txt");
+
+        using var t = new StreamReader(s);
+
+        LoadedResource = await t.ReadToEndAsync();        
+    }
+
+    [ObservableProperty]
+    private string? loadedResource;
+
+    private static async Task<Stream> OpenAppFileAsync(string path)
+    {
+        return await FileSystemEx.Current.OpenAppPackageFileAsync(path);
+    }
 }
+
+/// <summary>
+/// <see cref="Microsoft.Maui.Storage.FileSystem"/> with Desktop support
+/// </summary>
+class FileSystemEx : Microsoft.Maui.Storage.IFileSystem
+{
+    static FileSystemEx()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Current = new FileSystemEx();
+        }
+        else
+        {
+            Current = Microsoft.Maui.Storage.FileSystem.Current;            
+        }
+    }
+
+    public static Microsoft.Maui.Storage.IFileSystem Current { get; }
+
+    public FileSystemEx()
+    {
+        var path = Environment.ProcessPath;
+        var bytes = System.Text.Encoding.UTF8.GetBytes(path);
+        bytes = System.Security.Cryptography.MD5.HashData(bytes);
+        path = System.Convert.ToBase64String(bytes);
+
+        CacheDirectory = System.IO.Path.Combine(System.IO.Path.GetTempPath(), path);
+    }
+
+    public string CacheDirectory { get; }
+
+    public string AppDataDirectory => AppContext.BaseDirectory;
+
+    public async Task<Stream> OpenAppPackageFileAsync(string filename)
+    {
+        filename = System.IO.Path.Combine(AppDataDirectory, "Content", filename);
+
+        return await Task.FromResult(File.OpenRead(filename));
+    }
+
+    public async Task<bool> AppPackageFileExistsAsync(string filename)
+    {
+        filename = System.IO.Path.Combine(AppDataDirectory, "Content", filename);        
+
+        return await Task.FromResult(new FileInfo(filename).Exists);
+    }    
+}
+
+
+
+
 
 public partial class StringPair
 {
